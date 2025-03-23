3aea3aaa41ed44008b1fc1572d5f070a27f918e8
Mia Pedersen <miap@student.uia.no>
2025-03-23 20:20:18 +0100

    Created a function to solve the first task, and then posted it using the postanswer function

--- readme.md
+++ readme.md
@@ -13,3 +13,9 @@ Used the getTask function but swapped out 'start' for 'clue' in the request, did
 
 *postAnswer function:*
 Used the framework provided from node fetch and the website, although it hasnt produced any response yet.
+
+*solveTask1 function:*
+To solve the code I went to wikipedia's page on alchemy symbols: https://en.wikipedia.org/wiki/Alchemical_symbol
+It says that ☉ is for gold, ☿ is for mercury (quicksilver), ☽ is for silver, and ♂ is for iron
+I then split them into an array, and used a simple for loop to replace each one with their text option, before joining them together with commas inbetween
+
--- task.mjs
+++ task.mjs
@@ -2,7 +2,6 @@ import fetch from 'node-fetch';
 
 const URL = 'https://alchemy-kd0l.onrender.com';
 const PLAYER_NAME = 'miap@uia.no';
-const USER_ANSWER = 'answer';
 
 async function getTask() {
     const response = await fetch(`${URL}/start?player=${PLAYER_NAME}`);
@@ -17,11 +16,36 @@ async function askForClue() {
 }
 
 async function postAnswer() {
-    const response = await fetch(`${URL}/answer`, {
-        method: 'POST', body: JSON.stringify({
-            player: PLAYER_NAME, answer: USER_ANSWER
-        })
-    });
+    const response = await fetch(`${URL}/answer`,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ player: PLAYER_NAME, answer: USER_ANSWER })
+        });
     const data = await response.json();
     console.log(data);
 }
+
+function solveTask1() {
+    const CODE = "☉☿☽♂☉"
+    const CODEARRAY = CODE.split('');
+    for (let i = 0; i < CODE.length; i++) {
+        if (CODEARRAY[i] === '☉') {
+            CODEARRAY[i] = 'Gold';
+        }
+        else if (CODEARRAY[i] === '☿') {
+            CODEARRAY[i] = 'Quicksilver';
+        }
+        else if (CODEARRAY[i] === '☽') {
+            CODEARRAY[i] = 'Silver';
+        }
+        else if (CODEARRAY[i] === '♂') {
+            CODEARRAY[i] = 'Iron';
+        }
+    }
+    let answer = CODEARRAY.join(',');
+    return answer;
+}
+
+const USER_ANSWER = solveTask1();
+await postAnswer();
