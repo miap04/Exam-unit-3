da9102a5dd9d88d5c746c488e26ff40644594185
Mia Pedersen <miap@student.uia.no>
2025-03-23 17:43:59 +0100

    Added a getclue and postanswer function for use once I start solving the code

--- readme.md
+++ readme.md
@@ -5,4 +5,11 @@ On the first commit I installed the node fetch through npm
 
 **Task**
 
-When starting on the task I first imported the fetch from the installed node fetch, theni did a simple GET request to get the a response from the API
+*getTask function:*
+When starting on the task I first imported the fetch from the installed node fetch, then i did a simple GET request to get the a response from the API
+
+*askForClue function:*
+Used the getTask function but swapped out 'start' for 'clue' in the request, didnt get any response from the API though
+
+*postAnswer function:*
+Used the framework provided from node fetch and the website, although it hasnt produced any response yet.
--- task.mjs
+++ task.mjs
@@ -2,8 +2,26 @@ import fetch from 'node-fetch';
 
 const URL = 'https://alchemy-kd0l.onrender.com';
 const PLAYER_NAME = 'miap@uia.no';
+const USER_ANSWER = 'answer';
 
-const response = await fetch(`${URL}/start?player=${PLAYER_NAME}`);
-const data = await response.json();
+async function getTask() {
+    const response = await fetch(`${URL}/start?player=${PLAYER_NAME}`);
+    const data = await response.json();
+    console.log(data);
+}
 
-console.log(data);
+async function askForClue() {
+    const response = await fetch(`${URL}/clue?player=${PLAYER_NAME}`);
+    const data = await response.json();
+    console.log(data);
+}
+
+async function postAnswer() {
+    const response = await fetch(`${URL}/answer`, {
+        method: 'POST', body: JSON.stringify({
+            player: PLAYER_NAME, answer: USER_ANSWER
+        })
+    });
+    const data = await response.json();
+    console.log(data);
+}
